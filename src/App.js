4780a24d26dcbe61829aadb8a641bac84e970a76
ts-uc <s0@ts-uc.org>
2021-10-09 06:06:55 +0900

    React 4

--- src/App.js
+++ src/App.js
@@ -2,15 +2,20 @@
 
 import * as React from 'react'
 import './App.css'
+import React, { useState } from 'react';
 
 /**
  * 
  * @type {React.FC}
  */
 export const App = () => {
+  const [dogUrl ,setDogUrl] = useState("https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg");
   return (
     <div>
-      <h2>Hello, world!</h2>
+    <header>
+    </header>
+    <p>犬の画像を表示するサイトです</p>
+    <img src={dogUrl} />
     </div>
   )
 }
