d667860b0a9b04f0b3a4817fc9be141f277a12f2
ts-uc <s0@ts-uc.org>
2021-10-09 19:40:27 +0900

    station6

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // DO NOT DELETE
 
-import * as React from 'react'
+// import * as React from 'react'
 import './App.css'
 import React, { useState } from 'react';
 
@@ -10,13 +10,26 @@ import React, { useState } from 'react';
  */
 export const App = () => {
   const [dogUrl ,setDogUrl] = useState("https://images.dog.ceo/breeds/spaniel-brittany/n02101388_6057.jpg");
+  
+  const changeImage = () => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then(res => res.json())
+    .then(
+      (result) => {
+        setDogUrl(result.message);
+      },
+      (error) => {
+      }
+    )
+  };
+
   return (
     <div>
       <header>
       </header>
       <p>犬の画像を表示するサイトです</p>
       <img src={dogUrl} />
-      <button type="button" onClick={() => setDogUrl("https://images.dog.ceo/breeds/hound-english/n02089973_1132.jpg")}>更新</button>
+      <button type="button" onClick={changeImage}>更新</button>
     </div>
   )
 }
